ce0fd0b9fd0b17b387157c4368a6cbb99f03717f
Mark Broughton <mark.broughton@travisperkins.co.uk>
2026-09-08 13:21:15 +0100

    fixed duplication of references.

--- ServiceStatusChecker.Tests/Jobs/MorningReportJobTests.cs
+++ ServiceStatusChecker.Tests/Jobs/MorningReportJobTests.cs
@@ -18,21 +18,8 @@ public class MorningReportJobTests
     [Fact]
     public async Task Execute_CallsSendScheduledReportAsync()
     {
-        // Arrange: Create dummy dependencies to satisfy the base constructor
-        var stateConfig = Options.Create(new StateConfig());
-        var stateStore = new JsonStateStore(stateConfig, NullLogger<JsonStateStore>.Instance);
-        var reportStateStore = new MorningReportStateStore(stateConfig, NullLogger<MorningReportStateStore>.Instance);
-        var notifiers = Array.Empty<INotifier<MorningReportMessageContext>>();
+        var serviceMock = new Mock<IMorningReportService>();
 
-        var serviceMock = new Mock<MorningReportService>(
-            stateStore,
-            reportStateStore,
-            Options.Create(new MonitorConfigCollection()),
-            Options.Create(new MorningReportConfig()),
-            notifiers,
-            NullLogger<MorningReportService>.Instance);
-
-        // Setup the virtual method
         serviceMock.Setup(s => s.SendScheduledReportAsync())
             .Returns(Task.CompletedTask)
             .Verifiable();
@@ -41,10 +28,8 @@ public class MorningReportJobTests
         
         var job = new MorningReportJob(serviceMock.Object, NullLogger<MorningReportJob>.Instance);
 
-        // Act
         await job.Execute(contextMock.Object);
 
-        // Assert: Verify that the report trigger was fired
         serviceMock.Verify(s => s.SendScheduledReportAsync(), Times.Once);
     }
 }
